ad6fa8187d4fb16d9a014f6c0bf9e453136a7d03
Soshini2009 <yanosoushi@gmail.com>
2026-02-14 10:06:58 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import discord
+from discord.ext import commands
 import os
 
 TOKEN = os.getenv("TOKEN")
-print("ENV TOKEN =", TOKEN)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -99,5 +100,6 @@ async def on_message(message):
 
 
 bot.run(TOKEN)
+
 
 
